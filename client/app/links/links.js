38d620b2859cae5cd8a6f5a51c43c24a7133c6e5
spiritphyz <heytonyle@icloud.com>
2016-10-14 15:06:23 -0700

    Implement LinksController to invoke Links.getAll()

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,12 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+  $scope.data = {};
+
+  $scope.getAll = function() {
+    Links.getAll()
+    .then(function(resp) {
+      $scope.data.links = resp;
+    });
+  }();
 });
