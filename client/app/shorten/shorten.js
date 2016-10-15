1be035f41a1c6dd6a2328099fe9c22f3a2d40baf
spiritphyz <heytonyle@icloud.com>
2016-10-14 17:23:41 -0700

    Interactively show errors when urls are invalid, shows on a letter-by-letter basis

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,7 +1,21 @@
+var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+
+var isValidUrl = function(url) {
+  return url.match(rValidUrl);
+};
+
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   $scope.link = {};
+
+  $scope.$watch('newUrl', function() {
+    if ($scope.newUrl.length > 1 && !isValidUrl($scope.newUrl)) {
+      $scope.error = 'Invalid URL';      
+    } else {
+      $scope.error = '';
+    }
+  });
 
   $scope.addLink = function() {
     Links.addOne($scope.newUrl);
